0497803ac028294a2f575f02b38bc7944c4c5d79
Nodarski <nodarlaw@gmail.com>
2018-07-25 17:32:33 -0600

    server is connecting to the db, port8080 is displaying something

--- app.js
+++ app.js
@@ -7,13 +7,22 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var db = require("./models");
+
+var PORT = process.env.PORT || 8080;
+
+
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
+
+
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -27,6 +36,10 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
+app.get('/', function (req, res) {
+  res.render('index', { title: 'Hey', message: 'Hello there!' })
+})
+
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
@@ -36,6 +49,12 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+db.sequelize.sync({ force: true }).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
 });
 
 module.exports = app;
