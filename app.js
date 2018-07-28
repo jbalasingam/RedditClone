7e87afb2727661c011b129c959613b589db70d7d
Nodarski <nodarlaw@gmail.com>
2018-07-28 11:31:18 -0600

    fixed /r/ redirecting and title. removed .bin/www file.  changed npm start => (node app.js)"

--- app.js
+++ app.js
@@ -42,32 +42,11 @@ app.use(function (req, res, next) {
 });
 
 
-app.get('/', function (req, res) {
-  res.render('index', {
-    title: 'Hey',
-    message: 'Hello there!'
-  })
-})
-
-app.get('/testy', function (req, res) {
-  res.render('index', {
-    title: 'Hey',
-    message: 'Hello there!'
-  })
-})
 
 
 
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+
 
 db.sequelize.sync({
   force: true
